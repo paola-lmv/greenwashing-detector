87fc4d638be1f875eb87754c94ee5696118677a8
paola.lmv <paola.lemorvan@gmail.com>
2026-03-13 23:50:57 +0000

    build5

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/GreenwashingDetector/',
+  base: '/greenwashing-detector/',
   optimizeDeps: {
     exclude: ['pdfjs-dist']
   }
